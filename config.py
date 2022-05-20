fc96037f3c903c440d67ce1dc0383e4b9f32196b
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-05-20 14:03:18 +0300

    Create the User model and install SQLAlchemy

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://shalyne:12345@localhost/blogs'
 
 
 class ProdConfig(Config):
